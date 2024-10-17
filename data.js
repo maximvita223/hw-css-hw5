e81432b141a1745354b85860e1cc4397be2470c5
maximvita223 <109244209+maximvita223@users.noreply.github.com>
2024-10-17 23:26:54 +0300

    complete intermediate certification

--- data.js
+++ data.js
@@ -1,40 +1,58 @@
 const dataProduct = `
     [
         {
-            "url": "img/product/1.svg",
-            "head": "ELLERY X M'O CAPSULE",
-            "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-            "price": "$52.00"
+                "id": "1",
+                "url": "img/product/1.svg",
+                "head": "ELLERY X M'O CAPSULE",
+                "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda  Operandi.",
+                "price": "$300.00",
+                "color": "red",
+                "size": "Xl"
         },
             {
-            "url": "img/product/2.svg",
-            "head": "ELLERY X M'O CAPSULE",
-            "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-            "price": "$52.00"
+                "id": "2",
+                "url": "img/product/2.svg",
+                "head": "ELLERY X M'O CAPSULE",
+                "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda  Operandi.",
+                "price": "$552.00",
+                "color": "blue",
+                "size": "XXl"
         },
             {
-            "url": "img/product/3.svg",
-            "head": "ELLERY X M'O CAPSULE",
-            "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-            "price": "$52.00"
+                "id": "3",
+                "url": "img/product/3.svg",
+                "head": "ELLERY X M'O CAPSULE",
+                "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda  Operandi.",
+                "price": "$582.00",
+                "color": "green",
+                "size": "L"
         },
             {
-            "url": "img/product/4.svg",
-            "head": "ELLERY X M'O CAPSULE",
-            "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-            "price": "$52.00"
+                "id": "4",
+                "url": "img/product/4.svg",
+                "head": "ELLERY X M'O CAPSULE",
+                "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda  Operandi.",
+                "price": "$782.00",
+                "color": "black",
+                "size": "M"
         },
             {
-            "url": "img/product/5.svg",
-            "head": "ELLERY X M'O CAPSULE",
-            "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-            "price": "$52.00"
+                "id": "5",
+                "url": "img/product/5.svg",
+                "head": "ELLERY X M'O CAPSULE",
+                "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda  Operandi.",
+                "price": "$2.00",
+                "color": "yelow",
+                "size": "Xl"
         },
             {
-            "url": "img/product/6.svg",
-            "head": "ELLERY X M'O CAPSULE",
-            "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-            "price": "$52.00"
+                "id": "6",
+                "url": "img/product/6.svg",
+                "head": "ELLERY X M'O CAPSULE",
+                "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda  Operandi.",
+                "price": "$5.00",
+                "color": "white",
+                "size": "S"
         }
     ]
 `
